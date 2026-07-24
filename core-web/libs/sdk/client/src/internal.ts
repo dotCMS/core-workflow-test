2f4ab816442ea1237ff2da796fa36dbb4acb0ad1
Kevin Davila <144152756+KevinDavilaDotCMS@users.noreply.github.com>
2026-07-24 00:35:31 -0500

    Update export comment to reflect build attempt II (#520)
    To trigger action to publish sdk next tag
    
    ### Proposed Changes
    * change 1
    * change 2
    
    ### Checklist
    - [ ] Tests
    - [ ] Translations
    - [ ] Security Implications Contemplated (add notes if applicable)
    
    ### Additional Info
    ** any additional useful context or info **
    
    ### Screenshots
    Original             |  Updated
    :-------------------------:|:-------------------------:
    ** original screenshot **  |  ** updated screenshot **
    
    fixes #515

--- core-web/libs/sdk/client/src/internal.ts
+++ core-web/libs/sdk/client/src/internal.ts
@@ -1,2 +1,2 @@
-// This is the export. This comment is only to trigger a build.
+// This is the export. This comment is only to trigger a build. Attempt II.
 export { graphqlToPageEntity } from './lib/utils/graphql/transforms';
